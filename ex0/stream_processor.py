11f649d0bda151f888db3484fd417815496f5612
MANSIR Abderrahmane <mansir.0.abderrahmane@gmail.com>
2026-03-22 18:36:39 +0000

    ex0

--- ex0/stream_processor.py
+++ ex0/stream_processor.py
@@ -1,4 +1,4 @@
-from typing import Any, List, Dict, Union, Optional
+from typing import Any, List
 from abc import ABC, abstractmethod
 
 
@@ -22,9 +22,11 @@ class NumericProcessor(DataProcessor):
         return f"Processed data: {data}"
 
     def validate(self, data: Any) -> bool:
-        return isinstance(data, (List))
+        return isinstance(data, list)
 
     def format_output(self, data: List[int]) -> str:
+        if not self.validate(data):
+            raise ValueError("Invalid data for NumericProcessor")
         ln = len(data)
         sm = sum(x for x in data)
         ag = sm/ln
@@ -40,8 +42,12 @@ class TextProcessor(DataProcessor):
     def validate(self, data: Any) -> bool:
         return isinstance(data, str)
 
-    def format_output(self, result: str) -> str:
-        return f"Formatted output: {result}"
+    def format_output(self, data: str) -> str:
+        if not self.validate(data):
+            raise ValueError("Invalid data for TextProcessor")
+        ln = len(data)
+        sm = len(data.split())
+        return f"Processed text: {ln} characters, {sm} words"
 
 
 class LogProcessor(DataProcessor):
@@ -51,10 +57,17 @@ class LogProcessor(DataProcessor):
         return f"Processed log data: {data}"
 
     def validate(self, data: Any) -> bool:
-        return isinstance(data, str)
+        is_str = isinstance(data, str)
+        has_log_level = any(level in data for level in ["ERROR", "WARNING", "INFO"])
+        return is_str and has_log_level
 
-    def format_output(self, result: str) -> str:
-        return f"Formatted output: {result}"
+    def format_output(self, data: str) -> str:
+        if not self.validate(data):
+            raise ValueError("Invalid data for LogProcessor")
+        type = "ALERT" if "ERROR" in data else "WARNING" if "WARNING" in data else "INFO"
+        event = data.split(":")[0]
+        msg = data.split(":")[1]
+        return f"[{type}] {event} level detected:{msg}"
 
 
 def main() -> None:
@@ -63,23 +76,38 @@ def main() -> None:
     text_processor = TextProcessor()
     log_processor = LogProcessor()
 
-    numeric_data = [1, 2, 3, 4, 5]
-    text_data = "Hello Nexus World"
-    log_data = "ERROR: Connection timeout"
+    try:
+        numeric_data = [1, 2, 3, 4, 5]
+        print("Initializing Numeric Processor...")
+        print(numeric_processor.process(numeric_data))
+        print("Validation: Numeric data",
+              "verified" if numeric_processor.validate(numeric_data)
+              else "not verified")
+        print("Output:", numeric_processor.format_output(numeric_data), "\n")
+    except Exception as e:
+        print(f"An error occurred in Numeric Processor: {e}\n")
 
-    print("Initializing Numeric Processor...")
-    print(numeric_processor.process(numeric_data))
-    print("Validation: Numeric data ",
-          "verified" if numeric_processor.validate(numeric_data)
-          else "not verified")
-    print("Output: ", numeric_processor.format_output(numeric_data), "\n")
+    try:
+        text_data = "Hello Nexus World"
+        print("Initializing Text Processor...")
+        print(text_processor.process(text_data))
+        print("Validation: Text data",
+              "verified" if text_processor.validate(text_data)
+              else "not verified")
+        print("Output:", text_processor.format_output(text_data), "\n")
+    except Exception as e:
+        print(f"An error occurred in Text Processor: {e}\n")
 
-    print("Initializing Text Processor...")
-    print("Processing data: ", text_data)
-    print(text_processor)
-    print("Initializing Log Processor...")
-    print("Processing data: ", log_data)
-    print(log_processor)
+    try:
+        log_data = "WARNING: system overload detected"
+        print("Initializing Log Processor...")
+        print(log_processor.process(log_data))
+        print("Validation: Log data",
+              "verified" if log_processor.validate(log_data)
+              else "not verified")
+        print("Output:", log_processor.format_output(log_data), "\n")
+    except Exception as e:
+        print(f"An error occurred in Log Processor: {e}\n")
 
 
 if __name__ == "__main__":
